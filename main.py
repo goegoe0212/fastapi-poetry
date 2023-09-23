ee4b1e4a2c0e3033b13ed457824e8c8adadf5b05
yuki <goegoe0212@outlook.jp>
2023-09-23 12:38:21 +0000

    router処理を修正

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# Third Party Library
 from fastapi import FastAPI
+
+# First Party Library
 from routers import router_hello
 
 app = FastAPI()
 
-app.route(router_hello)
+app.include_router(router_hello.app)
--- routers/router_hello.py
+++ routers/router_hello.py
@@ -1,13 +1,17 @@
+# Third Party Library
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 
 app = APIRouter()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root() -> JSONResponse:
+    data = {"Hello": "World"}
+    return JSONResponse(data)
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int, q: str | None = None) -> JSONResponse:
+    data = {"item_id": item_id, "q": q}
+    return JSONResponse(data)
